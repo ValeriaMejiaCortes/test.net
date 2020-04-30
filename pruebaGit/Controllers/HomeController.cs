1c1004320083efd0cc4d8a81dbd778c7244fa498
ValeriaMejiaCortes <valeria.380172802@ucaldas.edu.co>
2020-04-30 01:47:10 -0500

    initial commit

--- pruebaGit/Controllers/HomeController.cs
+++ pruebaGit/Controllers/HomeController.cs
@@ -15,7 +15,7 @@ namespace pruebaGit.Controllers
 
         public ActionResult About()
         {
-            ViewBag.Message = "Your application description page.";
+            ViewBag.Message = "Your application description page...";
 
             return View();
         }
